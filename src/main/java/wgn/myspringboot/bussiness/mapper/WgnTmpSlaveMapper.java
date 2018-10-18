96dc2a8d6fa2513b6ab6029faec04a904ede0c81
wangguannan <guannan@mamahao.com>
2018-10-18 15:58:48 +0800

    测试多数据源

---
+++ src/main/java/wgn/myspringboot/bussiness/mapper/WgnTmpSlaveMapper.java
@@ -0,0 +1,12 @@
+package wgn.myspringboot.bussiness.mapper;
+
+
+import org.apache.ibatis.annotations.Mapper;
+import wgn.myspringboot.mybatis.IMapper;
+import wgn.myspringboot.bussiness.entity.WgnTmpEntity;
+
+/**
+ * WgnTmpEntity扩展
+ */
+public interface WgnTmpSlaveMapper extends IMapper<WgnTmpEntity> {
+}
